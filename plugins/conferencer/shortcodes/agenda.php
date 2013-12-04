b8776dd0bcfbfcb6f946c885ad97ad0d96dc4c94
Paul Côté <paul.cote@libeo.com>
2013-12-04 14:23:55 -0500

    Removed vardump

--- plugins/conferencer/shortcodes/agenda.php
+++ plugins/conferencer/shortcodes/agenda.php
@@ -391,8 +391,6 @@ class Conferencer_Shortcode_Agenda extends Conferencer_Shortcode {
                     }
                   }
                 }
-                echo date($row_time_format, $row_starts);
-                var_dump("$no_sessions && $rowspan_nosession == 0");echo '<br><br>';
                 if($no_sessions && $rowspan_nosession == 0){
                   $output .= '<td></td>';
                 }
