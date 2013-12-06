9ee50f51f925ede0dc63f2d2edb0f4d446b82331
Louis-Etienne Foy <louis-etienne.foy@gmail.com>
2013-12-06 13:52:39 -0500

    Updage schedule styles

---
+++ themes/waq2014/html/single.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="fr_CA"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="fr_CA"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="fr_CA"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr_CA"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Web à Québec 2014</title>
+    <meta name="description" content="Web à Québec 2014">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Lusitana:400,700|Roboto:400,300,500,700,900|Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <!-- Scripts -->
+    <script src="../js/vendor/jquery-1.10.2.min.js"></script>
+    <script src="../js/vendor/jquery.onfontresize.js"></script>
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="../js/vendor/respond.js"></script>
+
+</head>
+<body>
+
+    <!-- Page wrapper -->
+    <div class="l-page-wrapper single-page" itemscope itemtype="http://schema.org/Event">
+
+        <!-- Home section -->
+        <header class="home">
+            <div class="l-section-wrapper">
+
+                <a href="#" class="visuallyhidden focusable">Passer au contenu</a>
+
+                <h1 itemprop="name" class="visuallyhidden">Le Web à Québec</h1>
+
+                <div class="event-logo">
+                    <a href="#">
+                        <img src="../img/logo.png" alt="">
+                    </a>
+                </div>
+
+                <a href="#" class="event-back-link">Retour à l'horaire</a>
+                <a href="#" class="event-subscribe event-subscribe-menu">Achetez vos billets</a>
+
+            </div>
+        </header><!-- end of Home section -->
+
+
+        <!-- Page content section -->
+        <section id="partenaires" class="content">
+            <div class="l-section-wrapper">
+
+                <div class="l-row">
+                    <div class="l-col">
+
+
+
+                    </div>
+                    <div class="l-col">
+
+
+
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- end of sponsors section -->
+
+
+        <!-- Footer section -->
+        <footer class="footer">
+            <div class="l-section-wrapper">
+
+                <header class="visuallyhidden">
+                    <h2>Informations générales</h2>
+                </header>
+
+                <div class="l-row footer-infos">
+
+                    <div class="l-col">
+                        <div class="contact-element" itemscope itemtype="http://schema.org/Place">
+                            <span class="name" itemprop="name">Espace 400e</span>
+                            <span class="adresse" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                                <span class="streetAddress" itemprop="streetAddress">100, rue Quai St-André</span>
+                                <span class="addressLocality" itemprop="addressLocality">Québec</span> (QC) <span class="postalCode" itemprop="postalCode">G1K 3Y2</span>
+                            </span>
+                            <span class="link"><a href="#">Comment s'y rendre</a></span>
+                        </div>
+                    </div>
+
+                    <div class="l-col two-elements">
+                        <div class="contact-element" itemscope itemtype="http://schema.org/Organization">
+                            <span class="name" itemprop="name">Questions générales</span>
+                            <span class="telephone" itemprop="telephone">418 785-9874 #765</span>
+                            <span class="email" itemprop="email"><a href="#">info@webaquebec.org</a></span>
+                        </div>
+                        <div class="contact-element" itemscope itemtype="http://schema.org/Organization">
+                            <span class="name" itemprop="name">Vous êtes un médias</span>
+                            <span class="telephone" itemprop="telephone">418 704-1559 #228</span>
+                            <span class="email" itemprop="email"><a href="#">mediainfo@webaquebec.org</a></span>
+                        </div>
+                    </div>
+
+                    <div class="l-col">
+                        <div class="contact-element" itemscope itemtype="http://schema.org/Place">
+                            <span class="name" itemprop="name">Siège social</span>
+                            <span class="adresse" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                                <span class="streetAddress" itemprop="streetAddress">305, Charest Est, bureau 901</span>
+                                <span class="addressLocality" itemprop="addressLocality">Québec</span> (QC) <span class="postalCode" itemprop="postalCode">G1K 3H3</span>
+                                <span class="telephone" itemprop="telephone">418 647-3877</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="l-col large">
+                        <p class="strong">Suivez-nous sur le réseaux sociaux et collaborez avec nous</p>
+                        <p class="strong hashtag"><a href="#">#WAQ2014</a></p>
+                        <div class="social-medias">
+                            <a href="#" class="Facebook"><img src="../img/icon-social-facebook.png" alt="Facebook"></a>
+                            <a href="#" class="Twitter"><img src="../img/icon-social-twitter.png" alt="Twitter"></a>
+                            <a href="#" class="LinkedIn"><img src="../img/icon-social-linkedin.png" alt="LinkedIn"></a>
+                            <a href="#" class="Google+"><img src="../img/icon-social-googleplus.png" alt="Google+"></a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="l-row footer-bar">
+
+                    <div class="l-col archives">
+                        <h3>Archives</h3>
+                        <ul>
+                            <li><a href="#"><abbr title="Web à Québec">WAQ</abbr> <span class="visuallyhidden">édition</span> 2013</a></li>
+                            <li><a href="#"><abbr title="Web à Québec">WAQ</abbr> <span class="visuallyhidden">édition</span> 2012</a></li>
+                            <li><a href="#"><abbr title="Web à Québec">WAQ</abbr> <span class="visuallyhidden">édition</span> 2011</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="l-col right">
+                        <div><a href="#">Accessibilité</a></div>
+                        <div>
+                            <span class="strong">Conception et réalisation :</span>
+                            <a href="#" itemscope itemtype="http://schema.org/Organization">
+                                <span itemprop="name">Libéo</span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </footer><!-- end of Footer section -->
+
+    </div><!-- end of Page wrapper -->
+
+    <!-- Custom scripts -->
+    <script src="../js/main.js"></script>
+
+</body>
+</html>
