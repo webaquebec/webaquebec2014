db97d3515421c26b1c658d3a1a08ec8f71747eb4
Louis-Etienne Foy <louis-etienne.foy@gmail.com>
2013-12-09 09:26:55 -0500

    Add html single page

--- themes/waq2014/html/single.php
+++ themes/waq2014/html/single.php
@@ -56,12 +56,124 @@
                 <div class="l-row">
                     <div class="l-col">
 
+                        <article>
 
+                            <header class="content-header">
+                                <h1 itemprop="name">Ubiquitous Information Architecture: A Framework for Cross-Channel Strategy</h1>
+                                <div class="informations">
+                                    <span class="session-speaker" itemprop="performer" itemscope itemtype="http://schema.org/Person">
+                                        <span itemprop="name">Peter Morville</span>
+                                    </span>
+                                    <span class="meta">
+                                        <span class="date"><time datetime="2014-02-23">Jeudi 23 février</time></span>
+                                        <span class="time">
+                                            <time itemprop="startDate" datetime="2014-02-23 08:30">8 h 30</time> à
+                                            <time itemprop="endDate" datetime="2014-02-23 10:00">10 h 00</time>
+                                        </span>
+                                        <span class="location" itemprop="location" itemscope itemtype="http://schema.org/Place">
+                                            <span itemprop="address" itemscope itemtype="http://schema.org/Place">
+                                                <span itemprop="name">Grand hall</span>
+                                            </span>
+                                        </span>
+                                    </span>
+                                    <p class="language">La conférence sera donnée en anglais.</p>
+                                </div>
+                            </header>
+
+                            <div class="content">
+                                <p>Au carrefour entre l’informatique omniprésente et l’Internet, l’information rend floue la distinction entre les produits et services, permettant des expériences usager multicanal et à travers les médias. Cette interconnectivité offre une opportunité sans précédent de réimaginer l’architecture d’information. Jamais auparavant n’avons-nous pu mettre en oeuvre une telle combinaison de réseaux, d’appareils et de capteurs pour recueillir et partager l’information et créer des parcours significatifs pour l’usager. Lors de cette séance, nous ferons le pont entre l’architecture d’information classique et multi-réseaux.</p>
+                                <p>Nous porterons une attention spéciale à l’intégration des appareils mobiles et du social dans une “stratégie web” réactive et orientée vers le futur, tout en mettant en lumière les relations clé avec la stratégie de contenu, la trouvabilité, les analytics et la gouvernance. Nous explorerons aussi comment les cartes d’experience et la “pensée IA” peuvent améliorer le processus et le produit de l’architecture d’information et la conception de l’expérience usager.</p>
+                                <h2>Questions centrales</h2>
+                                <ul>
+                                    <li>Pourquoi l’architecture d’information est-elle encore plus importante à l’ère de l’expérience usager inter-plateforme, du mobile et du social?</li>
+                                    <li>Comment pouvons-nous appliquer la “pensée systémique” et la “pensée IA” afin d’améliorer nos produits et services?</li>
+                                    <li>Comment pouvons-nous concevoir des parcours qui englobent une gamme grandissante de points de contact physiques et numériques?</li>
+                                </ul>
+                                <h2>Thématiques de cette conférence</h2>
+                                <ul>
+                                    <li><a href="#">Design</a></li>
+                                    <li><a href="#">Mobile</a></li>
+                                    <li><a href="#">Médias</a></li>
+                                    <li><a href="#">UX</a></li>
+                                </ul>
+                            </div>
+
+                        </article>
+
+                        <a href="#" class="back-link">Retour à l'horaire</a>
 
                     </div>
                     <div class="l-col">
 
+                        <aside role="complementary">
+                            <h2>
+                                <span class="small">À propos de </span>
+                                <span class="name">Peter Morville</span>
+                            </h2>
 
+                            <div class="content">
+                                <p>Peter Morville est un écrivain, conférencier et consultant, connu pour avoir contribué à la création de la discipline de l’architecture d’information. Il a écrit plusieurs livres et conseille des clients tels que AT&T, Harvard, IBM, la librairie du Congrès, Microsoft, l’Institut national du cancer. On a parlé de ses travaux sur l’architecture d’information et l’expérience usager dans des médias tels que Business Week, The Economist, Fortune, NPR et The Wall Street Journal. Il tient un blog sur findability.org.</p>
+                                <p>Peter Morville is a writer, speaker, and consultant. He is best known for helping to create the discipline of information architecture.the National Cancer Institute, Vodafone, and the Weather Channel. His work on information architecture and user experience strategy has been covered by Business Week, The Economist, Fortune, NPR, and The Wall Street Journal. Peter lives in Ann Arbor, Michigan with his wife, two daughters, and a dog named Knowsy. He blogs at findability.org.</p>
+                            </div>
+
+                            <ul class="social">
+                                <li class="website"><a href="#">findability.org</a></li>
+                                <li class="twitter"><a href="#">@morville</a></li>
+                            </ul>
+                        </aside>
+
+                        <h2>Ces conférences pourraient vous intéresser : </h2>
+
+                        <ul>
+                            <li itemprop="subEvent" itemscope itemtype="http://schema.org/Event">
+                                <h3 class="session-title" itemprop="name">
+                                    <a href="#">Beyond mobile: where no geek has gone before </a>
+                                </h3>
+                                <span class="session-speaker" itemprop="performer" itemscope="" itemtype="http://schema.org/Person">
+                                    <span itemprop="name">Josh Clark</span>
+                                </span>
+                                <span class="session-date"><time datetime="2014-02-23">Jeudi 23 février</time></span>
+                                <span class="session-time-slot">
+                                    <span><time itemprop="startDate" datetime="2014-03-20">8:30</time></span> à
+                                    <span><time itemprop="endDate" datetime="2014-03-20">10:00</time></span>
+                                </span>
+                                <span class="session-room" itemprop="location" itemscope="" itemtype="http://schema.org/Place">
+                                    <span itemprop="name">Grand Hall</span>
+                                </span>
+                            </li>
+                            <li itemprop="subEvent" itemscope itemtype="http://schema.org/Event">
+                                <h3 class="session-title" itemprop="name">
+                                    <a href="#">Beyond mobile: where no geek has gone before </a>
+                                </h3>
+                                <span class="session-speaker" itemprop="performer" itemscope="" itemtype="http://schema.org/Person">
+                                    <span itemprop="name">Josh Clark</span>
+                                </span>
+                                <span class="session-date"><time datetime="2014-02-23">Jeudi 23 février</time></span>
+                                <span class="session-time-slot">
+                                    <span><time itemprop="startDate" datetime="2014-03-20">8:30</time></span> à
+                                    <span><time itemprop="endDate" datetime="2014-03-20">10:00</time></span>
+                                </span>
+                                <span class="session-room" itemprop="location" itemscope="" itemtype="http://schema.org/Place">
+                                    <span itemprop="name">Grand Hall</span>
+                                </span>
+                            </li>
+                            <li itemprop="subEvent" itemscope itemtype="http://schema.org/Event">
+                                <h3 class="session-title" itemprop="name">
+                                    <a href="#">Beyond mobile: where no geek has gone before </a>
+                                </h3>
+                                <span class="session-speaker" itemprop="performer" itemscope="" itemtype="http://schema.org/Person">
+                                    <span itemprop="name">Josh Clark</span>
+                                </span>
+                                <span class="session-date"><time datetime="2014-02-23">Jeudi 23 février</time></span>
+                                <span class="session-time-slot">
+                                    <span><time itemprop="startDate" datetime="2014-03-20">8:30</time></span> à
+                                    <span><time itemprop="endDate" datetime="2014-03-20">10:00</time></span>
+                                </span>
+                                <span class="session-room" itemprop="location" itemscope="" itemtype="http://schema.org/Place">
+                                    <span itemprop="name">Grand Hall</span>
+                                </span>
+                            </li>
+                        </ul>
 
                     </div>
                 </div>
