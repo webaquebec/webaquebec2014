6a178bcca79e42b551856f5a0761bd505ce2aed5
Paul Côté <paul.cote@libeo.com>
2013-12-09 11:03:19 -0500

    New html for session single

---
+++ themes/waq2014/header-page.php
@@ -0,0 +1,19 @@
+<!-- Home section -->
+<header class="home">
+    <div class="l-section-wrapper">
+
+        <a href="#" class="visuallyhidden focusable">Passer au contenu</a>
+
+        <h1 itemprop="name" class="visuallyhidden">Le Web à Québec</h1>
+
+        <div class="event-logo">
+            <a href="<?php echo get_bloginfo('url'); ?>">
+                <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="">
+            </a>
+        </div>
+
+        <a href="<?php echo get_bloginfo('url'); ?>" class="event-back-link">Retour à l'horaire</a>
+        <a href="#" class="event-subscribe event-subscribe-menu">Achetez vos billets</a>
+
+    </div>
+</header><!-- end of Home section -->
