95ae2151967f57160597290be22c84dc9dfc6f7f
Paul Côté <paul.cote@libeo.com>
2013-12-17 13:44:40 -0500

    Fix #22059

--- themes/waq2014/header.php
+++ themes/waq2014/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
+<html class="ie ie7" lang="fr">
 <![endif]-->
 <!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
+<html class="ie ie8" lang="fr">
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
+<html lang="fr">
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
