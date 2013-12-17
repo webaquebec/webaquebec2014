5aa5b487371beec3071a296659524df1d8305a5c
Paul Côté <paul.cote@libeo.com>
2013-12-17 13:09:27 -0500

    Fix single session page new styles

--- themes/waq2014/single-session.php
+++ themes/waq2014/single-session.php
@@ -19,20 +19,21 @@
       	foreach ($speakers_ids as $speaker_id) {
       	  $speaker_post = get_post(array_shift($speakers_ids));
       	  
-      	  $speakers_output .= '<h2><span class="small">À propos de </span><span class="name">'.$speaker_post->post_title.'</span></h2>';
+      	  $speakers_output .= '<h2><span class="small">À propos de </span><span class="big">'.$speaker_post->post_title.'</span></h2>';
       	  
-      	  $speakers_output .= (get_post_field('post_excerpt', $speaker_post->ID) ? get_post_field('post_excerpt', $speaker_post->ID) : get_post_field('post_content', $speaker_post->ID));
+      	  $speakers_output .= '<div class="content">'.(get_post_field('post_excerpt', $speaker_post->ID) ? apply_filters('the_content', get_post_field('post_excerpt', $speaker_post->ID)) : apply_filters('the_content', get_post_field('post_content', $speaker_post->ID))).'</div>';
+      	  
       	  
       	  $speakers_output .= '<ul class="social">';
-      	  $speaker_twitter_handle = str_replace('@', '', get_post_meta($speaker_post->ID, 'twitter_handle', true));
-      	  if(!empty($speaker_twitter_handle)){
-      	    $speakers_output .= '<li><a href="http://twitter.com/'.$speaker_twitter_handle.'">@'.$speaker_twitter_handle.'</a></li>';
-      	  }
-      	  
       	  $speaker_website = addhttp(get_post_meta($speaker_post->ID, 'website', true));
       	  $speaker_website_clean = rtrim(str_replace('www.', '', str_replace('https://', '', str_replace('http://', '', $speaker_website))), '/');
       	  if(!empty($speaker_website)){
-      	    $speakers_output .= '<li><a href="'.$speaker_website.'">'.$speaker_website_clean.'</a></li>';
+      	    $speakers_output .= '<li class="website"><a href="'.$speaker_website.'">'.$speaker_website_clean.'</a></li>';
+      	  }
+      	  
+      	  $speaker_twitter_handle = str_replace('@', '', get_post_meta($speaker_post->ID, 'twitter_handle', true));
+      	  if(!empty($speaker_twitter_handle)){
+      	    $speakers_output .= '<li class="twitter"><a href="http://twitter.com/'.$speaker_twitter_handle.'">@'.$speaker_twitter_handle.'</a></li>';
       	  }
       	  $speakers_output .= '</ul>';
       	  
@@ -43,20 +44,21 @@
   	  $speaker_post = get_post(array_shift($speakers_ids));
   	  $speaker_name = $speaker_post->post_title;
   	  
-  	  $speakers_output .= '<h2><span class="small">À propos de </span><span class="name">'.$speaker_post->post_title.'</span></h2>';
+  	  $speakers_output .= '<h2><span class="small">À propos de </span><span class="big">'.$speaker_post->post_title.'</span></h2>';
   	  
-  	  $speakers_output .= (get_post_field('post_excerpt', $speaker_post->ID) ? get_post_field('post_excerpt', $speaker_post->ID) : get_post_field('post_content', $speaker_post->ID));
+  	  $speakers_output .= '<div class="content">'.(get_post_field('post_excerpt', $speaker_post->ID) ? apply_filters('the_content', get_post_field('post_excerpt', $speaker_post->ID)) : apply_filters('the_content', get_post_field('post_content', $speaker_post->ID))).'</div>';
   	  
   	  $speakers_output .= '<ul class="social">';
-  	  $speaker_twitter_handle = str_replace('@', '', get_post_meta($speaker_post->ID, 'twitter_handle', true));
-  	  if(!empty($speaker_twitter_handle)){
-  	    $speakers_output .= '<li><a href="http://twitter.com/'.$speaker_twitter_handle.'">@'.$speaker_twitter_handle.'</a></li>';
-  	  }
   	  
   	  $speaker_website = addhttp(get_post_meta($speaker_post->ID, 'website', true));
   	  $speaker_website_clean = rtrim(str_replace('www.', '', str_replace('https://', '', str_replace('http://', '', $speaker_website))), '/');
   	  if(!empty($speaker_website)){
-  	    $speakers_output .= '<li><a href="'.$speaker_website.'">'.$speaker_website_clean.'</a></li>';
+  	    $speakers_output .= '<li class="website"><a href="'.$speaker_website.'">'.$speaker_website_clean.'</a></li>';
+  	  }
+  	  
+  	  $speaker_twitter_handle = str_replace('@', '', get_post_meta($speaker_post->ID, 'twitter_handle', true));
+  	  if(!empty($speaker_twitter_handle)){
+  	    $speakers_output .= '<li class="twitter"><a href="http://twitter.com/'.$speaker_twitter_handle.'">@'.$speaker_twitter_handle.'</a></li>';
   	  }
   	  $speakers_output .= '</ul>';
   	  unset($speaker_post);
@@ -65,7 +67,7 @@
   	$related_sessions_output = "";
   	if(!empty($related_sessions_ids)){
   	  $related_sessions_output .= "<h2>Ces conférences pourraient vous intéresser : </h2>";
-  	  $related_sessions_output .= "<ul>";
+  	  $related_sessions_output .= '<ul class="subevents">';
     	foreach ($related_sessions_ids as $related_session_id) {
     	  $related_session_post = get_post($related_session_id);
     	  $related_sessions_output .= '<li itemprop="subEvent" itemscope itemtype="http://schema.org/Event">';
@@ -83,14 +85,14 @@
     	  
     	  $related_session_starts = get_post_meta($time_slot_id, '_conferencer_starts', true);
     	  $related_session_ends = get_post_meta($time_slot_id, '_conferencer_ends', true);
-    	  $related_sessions_output .= '<span class="meta">';
-    	      $related_sessions_output .= '<span class="date"><time datetime="'.trim(strftime("%Y-%m-%d",$related_session_starts)).'">'.strftime("%A %e %B",$related_session_starts).'</time></span>';
-    	      $related_sessions_output .= '<span class="time">';
-    	          $related_sessions_output .= '<time itemprop="startDate" datetime="'.trim(strftime("%Y-%m-%d %H:%M",$related_session_starts)).'">'.trim(strftime("%k h %M",$related_session_starts)).'</time> à';
-    	          $related_sessions_output .= '<time itemprop="endDate" datetime="'.trim(strftime("%Y-%m-%d %H:%M",$related_session_ends)).'">'.trim(strftime("%k h %M",$related_session_ends)).'</time>';
-    	      $related_sessions_output .= '</span>';
-    	      $related_sessions_output .= '<span class="location" itemprop="location" itemscope itemtype="http://schema.org/Place">';
-    	          $related_sessions_output .= '<span itemprop="address" itemscope itemtype="http://schema.org/Place">';
+    	  $related_sessions_output .= '<span class="session-meta">';
+    	      $related_sessions_output .= '<span class="session-date"><time datetime="'.trim(strftime("%Y-%m-%d",$related_session_starts)).'">'.strftime("%A %e %B",$related_session_starts).'</time></span>'."\n";
+    	      $related_sessions_output .= '<span class="session-time-slot">'."\n";
+    	          $related_sessions_output .= '<time itemprop="startDate" datetime="'.trim(strftime("%Y-%m-%d %H:%M",$related_session_starts)).'">'.trim(strftime("%k:%M",$related_session_starts)).'</time> à';
+    	          $related_sessions_output .= '<time itemprop="endDate" datetime="'.trim(strftime("%Y-%m-%d %H:%M",$related_session_ends)).'">'.trim(strftime("%k:%M",$related_session_ends)).'</time>';
+    	      $related_sessions_output .= '</span>'."\n";
+    	      $related_sessions_output .= '<span class="session-room" itemprop="location" itemscope itemtype="http://schema.org/Place">';
+    	          $related_sessions_output .= '<span itemprop="address" itemscope itemtype="http://schema.org/Place">'."\n";
     	              $related_sessions_output .= '<span itemprop="name">'.get_the_title(get_post_meta($related_session_post->ID, '_conferencer_room', true)).'</span>';
     	          $related_sessions_output .= '</span>';
     	      $related_sessions_output .= '</span>';
@@ -105,9 +107,9 @@
   	$themes_output = "";
   	if(!empty($themes)){
   	  $themes_output .= "<h2>Thématiques de cette conférence</h2>";
-    	$themes_output .= "<ul>";
+    	$themes_output .= '<ul class="tags">';
     	foreach ($themes as $theme) {
-    	  $themes_output .= '<li>'.$theme->name.'</li>';
+    	  $themes_output .= '<li><span>'.$theme->name.'</span></li>';
     	}
   		$themes_output .= "</ul>";
   	}
@@ -120,7 +122,7 @@
   	  foreach ($session_lists as $list) {
   	    $lists_output .= "<h2>".$list['title']."</h2>";
   	    if(!empty($list['elements'])){
-  	      $lists_output .= "<ul>";
+  	      $lists_output .= '<ul class="questions">';
     	    foreach ($list['elements'] as $element) {
     	      $lists_output .= "<li>".$element['text']."</li>";
     	    }
@@ -144,7 +146,7 @@
                               <span class="session-speaker" itemprop="performer" itemscope itemtype="http://schema.org/Person">
                                   <span itemprop="name"><?php echo $speaker_name; ?></span>
                               </span>
-                              <span class="meta">
+                              <span class="session-meta">
                                   <span class="date"><time datetime="<?php echo trim(strftime("%Y-%m-%d",$session_start_unix)); ?>"><?php echo strftime("%A %e %B",$session_start_unix); ?></time></span>
                                   <span class="time">
                                       <time itemprop="startDate" datetime="<?php echo trim(strftime("%Y-%m-%d %H:%M",$session_start_unix)); ?>"><?php echo trim(strftime("%k h %M",$session_start_unix)); ?></time> à
@@ -170,12 +172,12 @@
 
                   </article>
 
-                  <a href="<?php echo get_bloginfo('url'); ?>" class="back-link">Retour à l'horaire</a>
+                  <div  class="back-link"><a href="<?php echo get_bloginfo('url'); ?>">Retour à l'horaire</a></div>
 
               </div>
-              <div class="l-col">
+              <div class="l-col sidebar">
 
-                  <aside role="complementary">
+                  <aside class="highlight" role="complementary">
                         <?php echo $speakers_output; ?>
                   </aside>
 
