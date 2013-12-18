cf9ffc6cbc73bcbad6e749d31ea57ddc5cae4c36
Paul Côté <paul.cote@libeo.com>
2013-12-18 09:01:41 -0500

    Fix 22105

--- themes/waq2014/single-session.php
+++ themes/waq2014/single-session.php
@@ -140,7 +140,7 @@
 
                   <article>
 
-                      <header class="content-header">
+                      <header class="content-header go-to-content">
                           <h1 itemprop="name"><?php the_title() ?></h1>
                           <div class="informations">
                               <span class="session-speaker" itemprop="performer" itemscope itemtype="http://schema.org/Person">
