c5c4b1f7a9dcee0f4a8478c0ae1dcb2ac66b5ca3
Paul Côté <paul.cote@libeo.com>
2013-12-18 09:00:12 -0500

    Fix 22105

--- themes/waq2014/header-page.php
+++ themes/waq2014/header-page.php
@@ -9,7 +9,7 @@ $headerHomePage= $headerHomePage[0];
 <header class="home">
     <div class="l-section-wrapper">
 
-        <a href="#" class="visuallyhidden focusable">Passer au contenu</a>
+        <a href="#" class="visuallyhidden focusable l-ally">Passer au contenu</a>
 
         <h1 itemprop="name" class="visuallyhidden">Le Web à Québec</h1>
 
