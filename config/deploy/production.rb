996b2e7e87c4d0e9e7fdbb45641a9ff020a6f0a0
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-01-16 15:02:46 -0500

    Update to new deployment server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
-set :deploy_to, '/home/waq/web/production/'
+set :deploy_to, '/home3/webaqueb/public_html/production/'
+set :tmp_dir, '/home3/webaqueb/tmp'
 
-server '192.241.243.186', user: 'waq', roles: %w{web app db}
+server '66.147.244.160', user: 'webaqueb', roles: %w{web app db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,5 @@
 set :stage, :staging
-set :deploy_to, '/home/waq/web/staging/'
+set :deploy_to, '/home3/webaqueb/public_html/staging/'
+set :tmp_dir, '/home3/webaqueb/tmp'
 
-server '192.241.243.186', user: 'waq', roles: %w{web app db}
+server '66.147.244.160', user: 'webaqueb', roles: %w{web app db}
