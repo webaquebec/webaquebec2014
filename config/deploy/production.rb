d2638ff7204951c1d7f9b0eb090a2844c1f19e19
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-01-21 18:23:41 -0500

    Update deployment path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-set :deploy_to, '/home3/webaqueb/public_html/production/'
+set :deploy_to, '/home3/webaqueb/webaquebec2014/production/'
 set :tmp_dir, '/home3/webaqueb/tmp'
 
 server '66.147.244.160', user: 'webaqueb', roles: %w{web app db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
-set :deploy_to, '/home3/webaqueb/public_html/staging/'
+set :deploy_to, '/home3/webaqueb/webaquebec2014/staging/'
 set :tmp_dir, '/home3/webaqueb/tmp'
 
 server '66.147.244.160', user: 'webaqueb', roles: %w{web app db}
