018e7728ed8e07840615a04989192d5b14fb9876
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-01-21 20:05:14 -0500

    Move script to real template

--- wordpress/wp-includes/theme-compat/footer.php
+++ wordpress/wp-includes/theme-compat/footer.php
@@ -26,16 +26,5 @@ _deprecated_file( sprintf( __( 'Theme without %1$s' ), basename(__FILE__) ), '3.
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
 		<?php wp_footer(); ?>
-    <div id="fb-root"></div>
-    <script>
-      // Load the SDK asynchronously
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "https://connect.facebook.net/en_US/all.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-    </script>
 </body>
 </html>
