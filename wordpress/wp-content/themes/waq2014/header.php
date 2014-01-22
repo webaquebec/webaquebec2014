db906577cfcc124b193c53666647de1008086a5a
lefoy <contact@louisetiennefoy.com>
2014-01-22 14:56:53 -0500

    Add initial-scale=1 to viewport meta

--- wordpress/wp-content/themes/waq2014/header.php
+++ wordpress/wp-content/themes/waq2014/header.php
@@ -11,7 +11,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<!-- Set the viewport width to device width for mobile -->
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
